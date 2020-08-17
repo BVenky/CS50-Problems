244cf4fb769705a088013ee85b75254c9dc628a7
BVenky <BVenky@users.noreply.github.com>
2020-08-17 17:50:16 +0000

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -201,7 +201,7 @@ bool is_tie(int min)
     bool check = false;
     for (int i = 0; i < candidate_count; i++)
     {
-        if (candidates[i].votes == min)
+        if (candidates[i].votes == min && !candidates[i].eliminated)
         {
             check = true;
         }
