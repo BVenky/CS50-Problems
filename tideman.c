a9f09d91120b6fd19ab73680cda99dec8c22231b
BVenky <BVenky@users.noreply.github.com>
2020-08-17 09:13:08 +0000

    automated commit by check50 [check50=True]

--- tideman.c
+++ tideman.c
@@ -221,12 +221,19 @@ void lock_pairs(void)
         arr[i] = false;
         if (i == pair_count - 1)
         {
+            bool lost = false;
             for (int j = 0; j < pair_count - 1; j++)
             {
-                if (pairs[i].winner == pairs[j].loser && arr[j] == true)
+                if (!lost && pairs[i].winner == pairs[j].loser)
+                {
+                    lost = true;
+                }
+
+                if (arr[j] == true)
                 {
                     arr[i] = true;
                     //printf("%i %i\n", i, j);
+
                 }
                 else
                 {
@@ -234,8 +241,13 @@ void lock_pairs(void)
                     break;
                 }
 
+
             }
-            if(!arr[i])
+            if (arr[i] && lost)
+            {
+
+            }
+            else
             {
                 locked[pairs[i].winner][pairs[i].loser] = true;
             }
