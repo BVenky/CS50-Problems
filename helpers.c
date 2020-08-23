021bdb0ab09e4e8c6964159ca24e914b2b1a9bdd
BVenky <BVenky@users.noreply.github.com>
2020-08-23 19:41:56 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -1,6 +1,7 @@
 #include "helpers.h"
 #include "math.h"
 #include <stdint.h>
+#include "stdio.h"
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
@@ -26,36 +27,37 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int w = 0; w < width; w++)
         {
-            uint8_t sepiaRed = round(.393 * image[h][w].rgbtRed + .769 * image[h][w].rgbtGreen + .189 * image[h][w].rgbtBlue);
-            uint8_t sepiaGreen = round(.349 * image[h][w].rgbtRed + .686 * image[h][w].rgbtGreen + .168 * image[h][w].rgbtBlue);
-            uint8_t sepiaBlue = round(.272 * image[h][w].rgbtRed + .534 * image[h][w].rgbtGreen + .131 * image[h][w].rgbtBlue);
-            if (sepiaRed > (uint8_t) 255)
+            int sepiaRed = round(.393 * image[h][w].rgbtRed + .769 * image[h][w].rgbtGreen + .189 * image[h][w].rgbtBlue);
+            int sepiaGreen = round(.349 * image[h][w].rgbtRed + .686 * image[h][w].rgbtGreen + .168 * image[h][w].rgbtBlue);
+            int sepiaBlue = round(.272 * image[h][w].rgbtRed + .534 * image[h][w].rgbtGreen + .131 * image[h][w].rgbtBlue);
+            if (sepiaRed > 255)
             {
-                sepiaRed = (uint8_t) 255;
+                sepiaRed = 255;
             }
-            else if (sepiaRed < (uint8_t) 0)
+            else if (sepiaRed < 0)
             {
-                sepiaRed = (uint8_t) 0;
+                sepiaRed = 0;
             }
-            if (sepiaGreen > (uint8_t) 255)
+            if (sepiaGreen > 255)
             {
-                sepiaGreen = (uint8_t) 255;
+                sepiaGreen = 255;
             }
-            else if (sepiaGreen < (uint8_t) 0)
+            else if (sepiaGreen < 0)
             {
-                sepiaGreen = (uint8_t) 0;
+                sepiaGreen = 0;
             }
-            if (sepiaBlue > (uint8_t) 255)
+            if (sepiaBlue > 255)
             {
-                sepiaBlue = (uint8_t) 255;
+                sepiaBlue = 255;
             }
-            else if (sepiaBlue < (uint8_t) 0)
+            else if (sepiaBlue < 0)
             {
-                sepiaBlue = (uint8_t) 0;
+                sepiaBlue = 0;
             }
-            image[h][w].rgbtBlue = sepiaBlue;
-            image[h][w].rgbtGreen = sepiaGreen;
-            image[h][w].rgbtRed = sepiaRed;
+
+            image[h][w].rgbtBlue = (uint8_t) sepiaBlue;
+            image[h][w].rgbtGreen = (uint8_t) sepiaGreen;
+            image[h][w].rgbtRed = (uint8_t) sepiaRed;
         }
     }
     return;
