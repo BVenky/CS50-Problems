c5a6108b19c2b6882012309a106f29e5f14629aa
BVenky <BVenky@users.noreply.github.com>
2020-08-23 19:33:08 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -29,9 +29,33 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             uint8_t sepiaRed = round(.393 * image[h][w].rgbtRed + .769 * image[h][w].rgbtGreen + .189 * image[h][w].rgbtBlue);
             uint8_t sepiaGreen = round(.349 * image[h][w].rgbtRed + .686 * image[h][w].rgbtGreen + .168 * image[h][w].rgbtBlue);
             uint8_t sepiaBlue = round(.272 * image[h][w].rgbtRed + .534 * image[h][w].rgbtGreen + .131 * image[h][w].rgbtBlue);
-            image[h][w].rgbtBlue = sepiaRed;
+            if (sepiaRed > (uint8_t) 255)
+            {
+                sepiaRed = 255;
+            }
+            else if (sepiaRed < (uint8_t) 0)
+            {
+                sepiaRed = 0;
+            }
+            if (sepiaGreen > (uint8_t) 255)
+            {
+                sepiaGreen = 255;
+            }
+            else if (sepiaGreen < (uint8_t) 0)
+            {
+                sepiaGreen = 0;
+            }
+            if (sepiaBlue > (uint8_t) 255)
+            {
+                sepiaBlue = 255;
+            }
+            else if (sepiaBlue < (uint8_t) 0)
+            {
+                sepiaBlue = 0;
+            }
+            image[h][w].rgbtBlue = sepiaBlue;
             image[h][w].rgbtGreen = sepiaGreen;
-            image[h][w].rgbtRed = sepiaBlue;
+            image[h][w].rgbtRed = sepiaRed;
         }
     }
     return;
